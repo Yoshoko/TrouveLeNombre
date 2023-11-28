3a4973f5ceea9fe8cd844b6b57f07feee5917451
AymericMartinez <aymeric.martinez@esd-mail.com>
2023-11-28 17:18:44 +0100

    Lien JS -> html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// création des "liens vers le html "
+
+const inputValue = document.getElementById("inputValue");
+const btnCheck = document.getElementById("btnCheck");
+const textTry = document.getElementById("textTry");
+const textResponse = document.getElementById("textResponse");
