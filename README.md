5aee22322ef77251668b2427663bbe8337ef6033
AymericMartinez <aymeric.martinez@esd-mail.com>
2023-11-28 19:01:24 +0100

     ajout javascript

---
+++ README.md
@@ -0,0 +1,19 @@
+# On écrit en Markdown
+
+- description du projet 
+- conseils d'installation
+- mode d'emploi
+- license
+- captures d'écrans
+- Historique de modifications
+- Infos sur le(s) Auteur(s)
+
+# todo list 
+
+- [x]html
+- [ ]js
+- [ ]js
+
+# Comment le mettre 
+
+- On écrit en Markdown
--- js/app.js
+++ js/app.js
@@ -4,3 +4,38 @@ const inputValue = document.getElementById("inputValue");
 const btnCheck = document.getElementById("btnCheck");
 const textTry = document.getElementById("textTry");
 const textResponse = document.getElementById("textResponse");
+
+function randInt(mini, maxi) {
+    return mini + Math.floor(maxi * Math.random());
+}
+
+// for (let i = 1; i < 100; i++) {
+//   console.log(randInt(1, 6));
+// }
+
+let computerNumber = randInt(1, 100);
+console.log(computerNumber);
+
+let guessTry = 10;
+
+btnCheck.onclick = function () {
+    let userNumber = Number(inputValue.value);
+    console.log(userNumber);
+
+    if (userNumber == computerNumber) {
+        textResponse.innerHTML = "Youpi";
+    } else {
+        if (userNumber > computerNumber) {
+            textResponse.innerHTML = "Trop Grand";
+        } else {
+            textResponse.innerHTML = "Trop Petit";
+        }
+        guessTry--;
+        textTry.innerHTML = "Il reste " + guessTry + " essais";
+        if (guessTry <= 0) {
+            textResponse.innerHTML = "Perdu , aucun essai restant";
+            btnCheck.disabled = true;
+        }
+    }
+
+}
